47be4cdea3d9284711ae49d1ae96291797b10794
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-10-13 13:44:05 +0200

    add file controller

---
+++ Controller/FileController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Svd\MediaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller
+ */
+class FileController extends Controller
+{
+    public function uploadAction(Request $request)
+    {
+        die;
+    }
+}
