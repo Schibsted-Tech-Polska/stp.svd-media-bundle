2203488af9892f12c72c40fbdde47c990ddb2596
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-10-14 14:42:37 +0200

    temporary upload images

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -4,7 +4,9 @@ namespace Svd\MediaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\FileBag;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -17,20 +19,32 @@ class FileController extends Controller
      * Upload action
      *
      * @param Request $request request
+     *
+     * @return JsonResponse
      */
     public function uploadAction(Request $request)
     {
         $files = $this->getFiles($request->files);
         $fs = new Filesystem();
+        $tmpFiles = [];
 
         /** @var UploadedFile $file */
         foreach ($files as $file) {
             $file->getPathname();
+            dump($file->getClientOriginalName());
             $tmpFilePath = sys_get_temp_dir() . '/' . md5(time());
             $fs->copy($file->getPathname(), $tmpFilePath);
+
+            $tmpFile = new File($tmpFilePath);
+
+            $tmpFiles[] = [
+                'pathname' => $tmpFile->getPathname(),
+                'originalName' => $file->getClientOriginalName(),
+                'originalExtension' => $file->getClientOriginalExtension(),
+            ];
         }
 
-        die;
+        return new JsonResponse($tmpFiles);
     }
 
     /**
