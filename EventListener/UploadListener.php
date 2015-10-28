a92323ed30085910d57ac7a7dcbbc7aec9ec6ad0
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-10-28 14:29:18 +0100

    [#1] decrease usages count if not use image for some entity

--- EventListener/UploadListener.php
+++ EventListener/UploadListener.php
@@ -3,6 +3,7 @@
 namespace Svd\MediaBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
 use Svd\MediaBundle\Entity\File;
 use Svd\MediaBundle\Manager\MediaManager;
 
@@ -11,7 +12,6 @@ use Svd\MediaBundle\Manager\MediaManager;
  */
 class UploadListener
 {
-
     /** @var MediaManager */
     protected $mediaManager;
 
@@ -41,6 +41,29 @@ class UploadListener
             $this->mediaManager->uploadFile($file);
             $file->setStatus(File::STATUS_ACTIVE);
             $file->setUsagesCount($file->getUsagesCount()+1);
+        }
+    }
+
+    /**
+     * On flush
+     *
+     * @param OnFlushEventArgs $event event
+     */
+    public function onFlush(OnFlushEventArgs $event)
+    {
+        $em = $event->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityUpdates() as $entity) {
+            foreach ($uow->getEntityChangeSet($entity) as $changeSet) {
+                if ($changeSet[0] instanceof File) {
+                    $oldFile = $changeSet[0];
+                    $oldFile->setUsagesCount($oldFile->getUsagesCount()-1);
+                    $md = $em->getClassMetadata('Svd\MediaBundle\Entity\File');
+                    $em->getUnitOfWork()->recomputeSingleEntityChangeSet($md, $oldFile);
+                    $em->persist($oldFile);
+                }
+            }
         }
     }
 }
