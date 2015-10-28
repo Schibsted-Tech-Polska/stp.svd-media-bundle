386a4eb0288b910a1cf7ab81c8055792939ba82d
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-10-28 15:16:35 +0100

    [#1] decreade usages count if entity containing files has been deleted

--- EventListener/UploadListener.php
+++ EventListener/UploadListener.php
@@ -2,8 +2,10 @@
 
 namespace Svd\MediaBundle\EventListener;
 
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\UnitOfWork;
 use Svd\MediaBundle\Entity\File;
 use Svd\MediaBundle\Manager\MediaManager;
 
@@ -57,13 +59,52 @@ class UploadListener
         foreach ($uow->getScheduledEntityUpdates() as $entity) {
             foreach ($uow->getEntityChangeSet($entity) as $changeSet) {
                 if ($changeSet[0] instanceof File) {
-                    $oldFile = $changeSet[0];
-                    $oldFile->setUsagesCount($oldFile->getUsagesCount()-1);
-                    $md = $em->getClassMetadata('Svd\MediaBundle\Entity\File');
-                    $em->getUnitOfWork()->recomputeSingleEntityChangeSet($md, $oldFile);
-                    $em->persist($oldFile);
+                    $this->decreaseUsagesCount($em, $changeSet[0]);
                 }
             }
         }
+
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            foreach ($this->findFilesInEntity($uow, $entity) as $file) {
+                $this->decreaseUsagesCount($em, $file);
+            }
+        }
+    }
+
+    /**
+     * Find files in entity
+     *
+     * @param UnitOfWork $uow    unit of work
+     * @param object     $entity entity
+     *
+     * @return array
+     */
+    protected function findFilesInEntity(UnitOfWork $uow, $entity)
+    {
+        $files = [];
+        $fields = $uow->getOriginalEntityData($entity);
+        if (count($fields) !== 0) {
+            foreach ($fields as $field) {
+                if ($field instanceof File) {
+                    $files[] = $field;
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Decrease usages count
+     *
+     * @param EntityManager $em   em
+     * @param File          $file file
+     */
+    protected function decreaseUsagesCount(EntityManager $em, File $file)
+    {
+        $file->setUsagesCount($file->getUsagesCount()-1);
+        $md = $em->getClassMetadata('Svd\MediaBundle\Entity\File');
+        $em->getUnitOfWork()->recomputeSingleEntityChangeSet($md, $file);
+        $em->persist($file);
     }
 }
