e9f43b4101caa3667cc92b9c80e455f67668e792
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-11-02 14:55:17 +0100

    [#2] download file for validation

--- Validator/Constraints/ImageValidator.php
+++ Validator/Constraints/ImageValidator.php
@@ -2,6 +2,7 @@
 
 namespace Svd\MediaBundle\Validator\Constraints;
 
+use Svd\MediaBundle\Manager\MediaManager;
 use Svd\MediaBundle\Model\File;
 use Svd\MediaBundle\Twig\MediaUrlExtension;
 use Symfony\Component\Validator\Constraint;
@@ -14,6 +15,9 @@ class ImageValidator extends Validator
 {
     /** @var MediaUrlExtension */
     protected $mediaUrlExtension;
+
+    /** @var MediaManager */
+    protected $mediaManager;
 
     /**
      * Set media url extension
@@ -30,12 +34,29 @@ class ImageValidator extends Validator
     }
 
     /**
+     * Set media manager
+     *
+     * @param MediaManager $mediaManager media manager
+     *
+     * @return self
+     */
+    public function setMediaManager(MediaManager $mediaManager)
+    {
+        $this->mediaManager = $mediaManager;
+
+        return $this;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function validate($value, Constraint $constraint)
     {
         if ($value instanceof File && $value->getStatus() == File::STATUS_WAITING) {
-            $imagePath = $this->mediaUrlExtension->getMediaUrl($value->getFilename());
+            $imageUrl = $this->mediaUrlExtension->getMediaUrl($value->getFilename());
+            $imagePath = $this->mediaManager->getTmpPath() . md5(microtime()) . $value->getFilename();
+            file_put_contents($imagePath, $imageUrl);
+
             parent::validate($imagePath, $constraint);
         }
     }
